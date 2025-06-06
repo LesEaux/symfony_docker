418cd721e7ad295559dcf19bdf2c4e742a3ceabd
Léo Vodable <vodable.leo@gmail.com>
2025-06-06 17:51:53 +0200

    feat: create files for users

---
+++ symfony/src/Form/UserForm.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email')
+            ->add('roles', ChoiceType::class, [
+                'label' => 'Rôles',
+                'choices' => [
+                    'Utilisateur'       => 'utilisateur',
+                    'Administrateur'    => 'admin',
+                    // Ajoute ici d’autres rôles que tu utilises, si besoin
+                ],
+                'multiple' => true,
+                'expanded' => true, // false si tu veux un <select> multiple, true pour checkboxes
+            ])
+            ->add('password')
+            ->add('nom')
+            ->add('prenom')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
